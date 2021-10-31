2fb0b2e33c535dca5d34b42211e8e426c932952b
Raj Patil <floatraj@gmail.com>
2021-10-31 18:36:26 +0530

    tried using few libraries to get config deafuled from aprent will leave it here

--- monitor/src/config/common.rs
+++ monitor/src/config/common.rs
@@ -1,8 +1,15 @@
 use bevy_reflect::Reflect;
+use bevy_reflect::Struct;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, Write};
+
+extern crate serde;
+extern crate serde_value;
+
+use serde::de::DeserializeOwned;
+use serde_value::Value;
 
 #[derive(Deserialize, Serialize)]
 pub struct Settings {
@@ -47,7 +54,7 @@ pub struct Groups {
     pub list: Vec<Services>,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Reflect)]
 pub struct Services {
     pub messages: String,
     pub priority: i32,
@@ -94,8 +101,35 @@ pub fn write_struct() {
         log: true,
     };
 
-    let item = Items {
-        label: "label".to_owned(),
+    let item1 = Items {
+        label: "item1".to_owned(),
+        target: "target".to_owned(),
+        priority: Some(2),
+        first_wait: Some(2),
+        wait_between: Some(2),
+        send_limit: Some(2),
+        item_sleep: Some(2),
+    };
+    let item2 = Items {
+        label: "item2".to_owned(),
+        target: "target".to_owned(),
+        priority: Some(2),
+        first_wait: Some(2),
+        wait_between: Some(2),
+        send_limit: Some(2),
+        item_sleep: Some(2),
+    };
+    let item3 = Items {
+        label: "item1".to_owned(),
+        target: "target".to_owned(),
+        priority: Some(2),
+        first_wait: Some(2),
+        wait_between: Some(2),
+        send_limit: Some(2),
+        item_sleep: Some(2),
+    };
+    let item4 = Items {
+        label: "item2".to_owned(),
         target: "target".to_owned(),
         priority: Some(2),
         first_wait: Some(2),
@@ -104,16 +138,25 @@ pub fn write_struct() {
         item_sleep: Some(2),
     };
     let service = Services {
-        messages: "message".to_owned(),
+        messages: "service1".to_owned(),
         priority: 2,
         first_wait: 2,
         wait_between: 2,
         send_limit: 2,
         item_sleep: 2,
-        items: vec![item],
+        items: vec![item1, item2],
+    };
+    let service2 = Services {
+        messages: "service2".to_owned(),
+        priority: 2,
+        first_wait: 2,
+        wait_between: 2,
+        send_limit: 2,
+        item_sleep: 2,
+        items: vec![item3, item4],
     };
     let group = Groups {
-        list: vec![service],
+        list: vec![service, service2],
     };
     let main = NotifyGeneral {
         notification: notification,
@@ -128,9 +171,47 @@ pub fn write_struct() {
 
     let path = "lines.json";
 
-    // let mut output = File::create(path).unwrap();
-    //fs::Write(output, serialized_setting.as_str()).unwrap();
     fs::write(path, &serialized_setting).expect("Unable to write file");
 
-    println!("{}", serialized_setting);
+    //println!("{}", serialized_setting);
+}
+
+impl Settings {
+    pub fn put_default_from_head(&mut self) {
+        for item in self.groups.list[0].items.iter_mut() {
+            let a = &item.label;
+            println!("from mut iter for {:?}", a);
+        }
+
+        // for (i, value: &Reflect) in self.groups.list[0].items[0].iter_fields().enumerate() {
+        //     let field_name = self.groups.list[0].items[0].name_at(i).unwrap();
+        //     println!("columns is {} and value is ", &field_name);
+        //     match value.downcast_ref::<u32>() {
+        //         Some(value) => println!("{} is a u32 with the value: {}", field_name, value),
+        //         None => println!("none"),
+        //     };
+        // }
+    }
+}
+
+fn get_field_by_name<T, R>(data: T, field: &str) -> R
+where
+    T: Serialize,
+    R: DeserializeOwned,
+{
+    let mut map = match serde_value::to_value(data) {
+        Ok(Value::Map(map)) => map,
+        _ => panic!("expected a struct"),
+    };
+
+    let key = Value::String(field.to_owned());
+    let value = match map.remove(&key) {
+        Some(value) => value,
+        None => panic!("no such field"),
+    };
+
+    match R::deserialize(value) {
+        Ok(r) => r,
+        Err(_) => panic!("wrong type?"),
+    }
 }
--- monitor/src/config/mod.rs
+++ monitor/src/config/mod.rs
@@ -3,7 +3,13 @@ mod common;
 
 use config::*;
 use eyre::WrapErr;
-use serde::Deserialize;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+extern crate serde;
+extern crate serde_value;
+
+use serde_value::Value;
+use std::fs;
 use std::path::Path;
 use tracing::{info, instrument};
 
@@ -40,5 +46,45 @@ impl AppConfig {
     #[instrument]
     pub fn from_setting() {
         common::write_struct();
+
+        let data = fs::read_to_string("read_setting.json").expect("Unable to read file");
+        let mut serialised: common::Settings = serde_json::from_str(data.as_str()).unwrap();
+
+        let f = serialised.groups.list[0].items[0].wait_between;
+        // match serialised.groups.list[0].items[0].first_wait {
+        //     Some(value) => {
+        //         println!("macth value is {}", &value);
+        //         value
+        //     }
+        //     None => 0,
+        // };
+        serialised.put_default_from_head();
+        let item = &serialised.groups.list[0].items[0];
+        let a: String = get_field_by_name(item, "priority");
+        println!("get field by name returned {}", &a);
+        println!("raed value is {:?}", &f);
+    }
+}
+
+fn get_field_by_name<T, R>(data: T, field: &str) -> R
+where
+    T: Serialize,
+    R: DeserializeOwned,
+{
+    let mut map = match serde_value::to_value(data) {
+        Ok(Value::Map(map)) => map,
+        _ => panic!("expected a struct"),
+    };
+
+    let key = Value::String(field.to_owned());
+    let value = match map.remove(&key) {
+        Some(value) => value,
+        None => panic!("no such field"),
+    };
+    println!("value is {:?}", &value);
+
+    match R::deserialize(value) {
+        Ok(r) => r,
+        Err(_) => panic!("wrong type?"),
     }
 }
