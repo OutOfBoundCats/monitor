18f0fff66f7d40498f61d1b6c682ceaec6aedb31
Raj Patil <floatraj@gmail.com>
2021-11-01 00:23:05 +0530

    added functionality to log traces to console and file

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -10,10 +10,31 @@ use crate::config::common::Settings;
 
 use crate::config::AppConfig;
 
+use tracing::subscriber::set_global_default;
+
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter, Registry};
+
 #[actix_web::main]
 async fn main() {
     let settings = Settings::from_setting();
 
+    LogTracer::init().expect("Failed to set logger");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let file_appender = tracing_appender::rolling::hourly("application_log", "application.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(fmt::Layer::default().with_writer(non_blocking))
+        .with(JsonStorageLayer)
+        //.with(formatting_layer)
+        ;
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
+    tracing::info!("hello");
     // define channel to controll actix thread
     let (tx, rx) = mpsc::channel();
     //read config from json
@@ -44,6 +65,7 @@ async fn main() {
 
     thread::spawn(|| loop {
         println!("hi number from the spawned thread!");
+        tracing::info!("hello");
         thread::sleep(Duration::from_millis(100));
     });
 
