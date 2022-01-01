de42b7a4ebb461fb494cca3466e7bda0e1dbb796
Raj Patil <floatraj@gmail.com>
2022-01-02 00:40:18 +0530

    made new memory monitor

--- monitor/src/monitors/memory.rs
+++ monitor/src/monitors/memory.rs
@@ -2,7 +2,10 @@ use std::{convert::TryInto, sync::Arc, thread};
 
 use systemstat::{saturating_sub_bytes, ByteSize, Platform};
 
-use crate::{monitors::thread_sleep, notifications::read_google_config::GoogleChatConfig};
+use crate::{
+    config::common::Settings, monitors::thread_sleep,
+    notifications::read_google_config::GoogleChatConfig,
+};
 
 pub fn memory_usage() -> (u64, u64, bool) {
     let sys = systemstat::System::new();
@@ -32,59 +35,183 @@ pub fn memory_usage() -> (u64, u64, bool) {
 //starts memory monitoring
 #[tracing::instrument(skip())]
 pub fn memory_monitor(google_chat_config: Arc<GoogleChatConfig>, settings: Settings) {
-    let google_chat_mutex = google_chat;
+    tracing::info!("Started Memory Monitor");
 
-    let mut notified: bool = false;
+    let inactive_days = settings.main.general.inactive_days;
+    let inactive_times = settings.main.general.inactive_times;
+    let notified: bool = false;
+    let mut msg_index: i32;
     let mut notification_count = 0;
+    let mut send_limit: i32;
+
+    match settings.groups.memory.send_limit {
+        Some(value) => {
+            send_limit = value;
+        }
+        None => {
+            send_limit = settings.main.notification.send_limit;
+        }
+    }
 
     loop {
         let severity = 2;
-        tracing::info!("Memory monitor loop");
-        thread_sleep(&inactive_times, &inactive_days);
 
-        let item_sleep_mili = &item.item_sleep * 1000;
+        //sleep thread if current time falls between inactive time specified in json config
+        thread_sleep(&inactive_times, &inactive_days);
+        let item_sleep_mili: i32;
+        match settings.groups.memory.item_sleep {
+            Some(value) => {
+                item_sleep_mili = value * 1000;
+            }
+            None => {
+                tracing::error!("Error in getting the cpu group item_sleep time");
+            }
+        }
 
         let (used_memory, total_memory, err) = memory_usage();
-        let mut memory_usage: i64 = 0;
-        if err == false {
-            memory_usage = ((used_memory / total_memory) * 100).try_into().unwrap();
-        } else {
-            memory_usage = -2;
-        }
 
-        tracing::info!("memory usage is {}", &memory_usage);
-        if memory_usage > 10 && notification_count <= item.send_limit {
-            let message = google_chat_mutex.build_msg(
-                &item,
-                "ERROR",
-                severity,
-                format!("Memory usage very high at {} ", &memory_usage),
-            );
+        let memory_used_percentage = used_memory / total_memory * 100;
 
-            let res = google_chat_mutex.send_chat_msg(message);
-
-            notified = true;
-            notification_count = notification_count + 1;
-            if notification_count == 1 {
-                thread::sleep(std::time::Duration::from_millis(
-                    (item.first_wait * 1000).try_into().unwrap(),
-                ));
-            } else {
-                thread::sleep(std::time::Duration::from_millis(
-                    (item.wait_between * 1000).try_into().unwrap(),
-                ));
+        //find which item with max threashold is getting crossed
+        let vec_index: Vec<i32> = vec![];
+        let vec_value: Vec<i32> = vec![];
+        let mut i = 0;
+        for item in settings.groups.memory.items {
+            if memory_used_percentage > item.target.parse().unwrap() {
+                vec_index.push(i);
+                let target = item.target.clone().parse().unwrap();
+                vec_value.push(target);
             }
 
-            tracing::error!("Memory usage very high at {} ", &memory_usage);
-        } else {
-            notification_count = 0;
-            notified = false;
+            i = i + 1;
+        }
+        //check the maximum threadshold which is gettign breached by current cpu usage
+        let mut max_value: i32 = -1;
+        match vec_value.iter().max() {
+            Some(value) => {
+                max_value = *value;
+            }
+            None => {
+                max_value = -1;
+            }
         }
 
-        if notified == false {
-            thread::sleep(std::time::Duration::from_millis(
-                item_sleep_mili.try_into().unwrap(),
-            ));
+        if max_value != -1 {
+            let max_index = vec_value.iter().position(|&r| r == max_value).unwrap();
+            let item_index: usize = vec_index[max_index].try_into().unwrap();
+
+            //got the item with highest threshold being croseed by cpu usage
+            let l_item = settings.groups.memory.items[item_index];
+
+            let mut l_first_wait;
+            match l_item.first_wait {
+                Some(value) => {
+                    l_first_wait = value;
+                }
+                None => {
+                    l_first_wait = settings.main.notification.first_wait;
+                }
+            };
+
+            let mut l_wait_between;
+            match l_item.wait_between {
+                Some(value) => {
+                    l_wait_between = value;
+                }
+                None => {
+                    l_wait_between = settings.main.notification.wait_between;
+                }
+            };
+
+            //for 1st msg wait for first wait
+            if notified == false {
+                msg_index = 0; //select negative msg from array
+                severity = 2; //inform employees
+
+                let l_msg = google_chat_config.build_msg(
+                    severity,
+                    settings.groups.memory.messages,
+                    msg_index,
+                    settings.groups.memory.priority,
+                    Some(l_item.label),
+                    Some(l_item.target),
+                );
+
+                google_chat_config.send_chat_msg(l_msg);
+
+                notified = true;
+
+                notification_count = notification_count + 1; // increament notification count
+
+                thread::sleep(std::time::Duration::from_millis(
+                    (l_first_wait * 1000).try_into().unwrap(),
+                ));
+            } else if notified == true && notification_count <= send_limit {
+                msg_index = 0; //select negative msg from array
+                severity = 2; //inform employees
+
+                let l_msg = google_chat_config.build_msg(
+                    severity,
+                    settings.groups.memory.messages,
+                    msg_index,
+                    settings.groups.memory.priority,
+                    Some(l_item.label),
+                    Some(l_item.target),
+                );
+
+                google_chat_config.send_chat_msg(l_msg);
+
+                notified = true;
+
+                notification_count = notification_count + 1; // increament notification count
+
+                thread::sleep(std::time::Duration::from_millis(
+                    (l_wait_between * 1000).try_into().unwrap(),
+                ));
+            } else if notified == true && notification_count > send_limit {
+                //notify management
+
+                msg_index = 0; // select negative msg from array
+                severity = 1; //inform management
+
+                let l_msg = google_chat_config.build_msg(
+                    severity,
+                    settings.groups.memory.messages,
+                    msg_index,
+                    settings.groups.memory.priority,
+                    Some(l_item.label),
+                    Some(l_item.target),
+                );
+
+                google_chat_config.send_chat_msg(l_msg);
+
+                //for subsequent messages wait for wait between
+                thread::sleep(std::time::Duration::from_millis(
+                    (l_wait_between * 1000).try_into().unwrap(),
+                ));
+
+                notification_count = 0;
+
+                notified = true;
+            }
+        }
+
+        //if suers were already notified before and cpu usage is normal now notify with +ve message
+
+        if max_value == 1 && notified == true {
+            notified = false;
+            notification_count = 0;
+            severity = 2;
+            msg_index = 1; // select positive msg from array
+
+            let l_msg = google_chat_config.build_msg(
+                severity,
+                settings.groups.memory.messages,
+                msg_index,
+                settings.groups.memory.priority,
+                None,
+                None,
+            );
         }
     }
 }
